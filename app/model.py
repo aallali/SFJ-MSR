6e1454a7608245d9013f79c6262f1d5a4135da02
Abdellah Allali <contact@allali.me>
2021-10-18 18:07:43 +0100

    Update model.py

--- app/model.py
+++ app/model.py
@@ -23,8 +23,8 @@ class UserSchema(BaseModel):
     class Config:
         schema_extra = {
             "example": {
-                "fullname": "Abdulazeez Abdulazeez Adeshina",
-                "email": "abdulazeez@x.com",
+                "fullname": "Abdellah Allali",
+                "email": "hello@mama.sita",
                 "password": "weakpassword"
             }
         }
@@ -36,7 +36,7 @@ class UserLoginSchema(BaseModel):
     class Config:
         schema_extra = {
             "example": {
-                "email": "abdulazeez@x.com",
+                "email": "hello@mama.sita",
                 "password": "weakpassword"
             }
         }
